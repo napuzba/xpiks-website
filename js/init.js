60c29817e46c64d4d8a0320f34c9f1340a26714a
Taras Kushnir <505555+ribtoks@users.noreply.github.com>
2018-01-11 13:42:35 +0200

    Update init.js

--- js/init.js
+++ js/init.js
@@ -81,6 +81,10 @@ $(document).ready(function() {
 	$('.tutorials-sidebar h3').click(function(){
 		$(this).closest('.tutorials-sidebar').toggleClass('close');
 	});
+	
+	$('.tutorial-nav > li > a').click(function(){
+		$(this).next().toggleClass('topic-open');
+	});
 	Accordion.init();
 	DownloadTabs.init(getOS());
 });
